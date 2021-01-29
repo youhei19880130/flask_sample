bbe934126f5c0292bc1e0ab8a5c000c42966f00a
yohhei someya <youhei19880130@yahoo.co.jp>
2021-01-29 21:49:13 +0900

    Modify some files

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = "Hello World"
-    return name
+def index():
+    return 'hello, world'
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
-
-## おまじない
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
